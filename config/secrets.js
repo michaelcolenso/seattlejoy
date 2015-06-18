4c2129abe4b8e7bafd95ef4524c2c1c03bc67861
Michael Colenso <github@michaelcolenso.com>
2015-06-18 07:32:07 -0700

    secrets

--- config/secrets.js
+++ config/secrets.js
@@ -20,7 +20,7 @@ module.exports = {
 
   db: process.env.MONGO_URL || 'mongodb://localhost:27017/seattleJoy',
 
-  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+  sessionSecret: process.env.SESSION_SECRET || 'stevefromcanada',
 
   mailgun: {
     user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
