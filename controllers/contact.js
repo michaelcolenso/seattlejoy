50067faf26cd1b23b0e9789da691265fe9d181fc
Michael Colenso <github@michaelcolenso.com>
2015-06-21 16:02:02 -0700

    contact email

--- controllers/contact.js
+++ controllers/contact.js
@@ -42,7 +42,7 @@ exports.postContact = function(req, res) {
   var from = req.body.email;
   var name = req.body.name;
   var body = req.body.message;
-  var to = 'your@email.com';
+  var to = 'admin@seattlejoy.com';
   var subject = 'Contact Form | Hackathon Starter';
 
   var mailOptions = {
